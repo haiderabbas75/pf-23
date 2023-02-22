0a1a9c97cabdc3cc5f4734d07f55f794756bb90b
haiderabbas75 <126060996+haiderabbas75@users.noreply.github.com>
2023-02-22 00:51:42 -0800

    Add files via upload

---
+++ PF23.py
@@ -0,0 +1,64 @@
+# Task 1
+int1 = 44
+int2 = 34
+int3 = 23
+int4 = 55
+int5 = 43
+flt1 = 3.5
+flt2 = 4.6
+flt3 = 5.6
+flt4 = 2.1
+flt5 = 4.3
+cmp1 = (1 + 4j)
+cmp2 = 3j
+cmp3 = 2j
+cmp4 = 7j
+cmp5 = 3j
+print(type(int1))
+print(type(int2))
+print(type(int3))
+print(type(int4))
+print(type(int5))
+print(type(flt1))
+print(type(flt2))
+print(type(flt3))
+print(type(flt4))
+print(type(flt5))
+print(type(cmp1))
+print(type(cmp2))
+print(type(cmp3))
+print(type(cmp4))
+print(type(cmp5))
+# Task 2
+my_name = 'haider abbas'
+father = 'M yasin'
+Mother = 'Sakina'
+print(type(my_name))
+print(type(father))
+print(type(Mother))
+
+
+
+Introductory_paragraph = 'My name is haider abbas. I belong to Gil-git and i am a high school graduate '
+
+
+#task1: Since the emlopyees are permanent we will use Tuple to create a collection
+
+Formercreeps_permenant_employees = ("Ali", "Asad", "Farah", "Shahmeer", "Subhan", "Salar", "Hafsa", "Sana",
+                                    "Hoora", "Ahad", "Saif", "Maryam", "Irfan", "Furqan", "Mehrooz")
+print(Formercreeps_permenant_employees)
+
+#task2: I will create the collection using list or set beacause i have to update the collection on monthly basis.
+
+#task3:
+products_in_stock = ["Ventilattor", "CTscan", "Defibrillator", "ECG machine", "Patient monitor", "Syringes", "MRI"]
+print(products_in_stock)
+#The Hoorpharma manager will decline the proposal stating that he has already signed an agreement with Seimens.
+
+
+
+
+
+
+
+
---
+++ PF_23_task4.py
@@ -0,0 +1,21 @@
+
+
+#Task 1
+customer_names = { "name": ["Hiader", "Ali", "Ahmed", "Sally", " Oliver", "Sansa", "Maria", "Basit", "Aliya", "Zehra"]}
+customer_height = {"height": [5.6, 6.0, 5.3, 5.7, 5.9, 6.2, 7.0, 5.4, 5.2, 5.8]}
+customer_contact = {"contact": [45634, 55467, 45536, 67764, 23435, 55464,65768,22344, 54546,65768 ]}
+print(customer_contact)
+print(customer_height)
+print(customer_names)
+
+#Task 3
+print("login")
+Email_or_phon_number = input('Enter your email or phone number : ')
+Password = int(input('Enter your password : '))
+
+#Task 2
+print("Registration Form")
+First_name = input('First Name : ')
+Lat_name = input('Last Name : ')
+Email = input('Email : ')
+Phone_number = int(input(' Phone Number : '))
