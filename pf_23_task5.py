a72f80ce7c07fb8efd844fdd1493321a21fb5e43
haiderabbas75 <126060996+haiderabbas75@users.noreply.github.com>
2023-02-22 01:05:24 -0800

    Delete pf_23_task5.py

--- pf_23_task5.py
+++
@@ -1,8 +0,0 @@
-
-num1 = int(input("Enter the first number : "))
-num2 = int(input("Enter the second number : "))
-
-print("Addition:", num1 + num2)
-print("Subtraction:", num1 - num2)
-print("Multiplication:", num1 * num2)
-print("Floor Division:", num1 // num2)
